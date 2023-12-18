f80f10f6df71304e2554d0dc8cd435737e98fcc7
goraxe <goraxe@gmail.com>
2023-12-18 02:31:27 -0800

    test: refactor to current yeoman-test

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,36 +1,51 @@
 "use strict";
-const path = require("path");
-const assert = require("yeoman-assert");
-const helpers = require("yeoman-test");
-const fs = require("fs-extra");
+// Const path = require("path");
+import path from "path";
+import assert from "yeoman-assert";
+import helpers from "yeoman-test";
+import fs from "fs-extra";
+
+// Const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+import app from "../generators/app";
 
 const badge =
   "[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)";
 
 describe("generator-semantic-release:app", () => {
-  it("creates .releaserc", () => {
-    return helpers.run(path.join(__dirname, "../generators/app")).then(() => {
-      assert.file([".releaserc"]);
-      assert.jsonFileContent(".releaserc", {
-        plugins: [
-          "@semantic-release/commit-analyzer",
-          "@semantic-release/release-notes-generator",
-          "@semantic-release/changelog",
-          "@semantic-release/git"
-        ]
+  /*  BeforeEach(async () => {
+    runResult = await helpers.create("app", {},{})
+  })
+  */
+  it("creates .releaserc", async () => {
+    return helpers
+      .create(app, {}, {})
+      .run()
+      .then(() => {
+        assert.file([".releaserc"]);
+        assert.jsonFileContent(".releaserc", {
+          plugins: [
+            "@semantic-release/commit-analyzer",
+            "@semantic-release/release-notes-generator",
+            "@semantic-release/changelog",
+            "@semantic-release/git"
+          ]
+        });
       });
-    });
   });
 
-  it("updates existing .releaserc", () => {
+  it("updates existing .releaserc", async () => {
     return helpers
-      .run(path.join(__dirname, "../generators/app"))
+      .create(app, {}, {})
+      .withPrompts({})
       .inTmpDir(dir => {
         fs.writeJson(path.join(dir, ".releaserc"), { branches: ["master"] });
       })
-      .then(dir => {
-        assert.file(path.join(dir, ".releaserc"));
-        assert.jsonFileContent(".releaserc", {
+      .run()
+      .then(runResult => {
+        runResult.assertFile(".releaserc");
+        // Assert.file(path.join(dir, ".releaserc"));
+        runResult.assertJsonFileContent(".releaserc", {
           branches: ["master"],
           plugins: [
             "@semantic-release/commit-analyzer",
@@ -43,34 +58,45 @@ describe("generator-semantic-release:app", () => {
   });
 
   describe("handles readme", () => {
-    it("updates with a button when there is no button", () => {
+    it("updates with a button when there is no button", async () => {
       return helpers
-        .run(path.join(__dirname, "../generators/app"))
+        .create(app, {}, {})
         .inTmpDir(dir => {
           fs.writeFile(path.join(dir, "README.md"), "content");
         })
-        .then(dir => {
-          assert.file(path.join(dir, "README.md"));
-          assert.fileContent("README.md", badge);
-          assert.fileContent("README.md", "content");
+        .run()
+        .then(runResult => {
+          /*
+          RunResult.inTmpDir((dir) => {
+            fs.writeFile(path.join(dir, "README.md"), "content");
+          })
+          */
+          runResult.assertFile("README.md");
+          runResult.assertFileContent("README.md", badge);
+          runResult.assertFileContent("README.md", "content");
+          //   });
         });
     });
 
-    it("does not create a README.md", () => {
-      return helpers.run(path.join(__dirname, "../generators/app")).then(() => {
-        assert.noFile("README.md");
-      });
+    it("does not create a README.md", async () => {
+      return helpers
+        .create(app, {}, {})
+        .run()
+        .then(runResult => {
+          runResult.assertNoFile("README.md");
+        });
     });
 
-    it("leaves well alone if there is already a button", () => {
+    it("leaves well alone if there is already a button", async () => {
       return helpers
-        .run(path.join(__dirname, "../generators/app"))
+        .create(app, {}, {})
         .inTmpDir(dir => {
           fs.writeFile(path.join(dir, "README.md"), badge);
         })
-        .then(dir => {
-          assert.file(path.join(dir, "README.md"));
-          assert.equalsFileContent("README.md", badge);
+        .run()
+        .then(runResult => {
+          runResult.assertFile("README.md");
+          runResult.assertEqualsFileContent("README.md", badge);
         });
     });
   });
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,7 @@
 "use strict";
-const Generator = require("yeoman-generator");
-const chalk = require("chalk");
-const yosay = require("yosay");
+import Generator from "yeoman-generator";
+import chalk from "chalk";
+import yosay from "yosay";
 
 const releasrc = {
   plugins: [
@@ -15,7 +15,7 @@ const releasrc = {
 const badge =
   "[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)";
 
-module.exports = class extends Generator {
+export default class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
@@ -54,4 +54,4 @@ module.exports = class extends Generator {
   end() {
     this.config.save();
   }
-};
+}
